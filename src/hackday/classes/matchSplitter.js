bc647ed9006415c392d524590efa83a2da2b176b
Tom Howe <tom.howe@bbc.co.uk>
2013-12-19 17:57:46 +0000

    started pass segmentation

--- src/hackday/classes/matchSplitter.js
+++ src/hackday/classes/matchSplitter.js
@@ -12,42 +12,58 @@ function matchSplitter() {
 	function execute(matchEvents) {
 
         var matchBlocks = []; 
-
-
-        var curBlock = {touches:[]};
-        curBlock["team_id"] = 0;
-
+        var curBlock = {touches:[],_team_id:0};
 
         for (var i=0;i<matchEvents.Event.length; i++) {
+
             var e = matchEvents.Event[i];
 
+            // get rid of unhelpful events
+            var t = e._type_id;
+            // start, start, deleted event, 
+            if (t == 32 || t==34 || t==43) {
+                continue;
+            }
+
+            //assign team_id to current block, if it's the first pass
+            if (curBlock.touches.length <= 2 && e._type_id == 1 && e._outcome) {
+               //  curBlock._team_id = e._team_id;
+
+            }
 
             // iterate through qualifiers
             if (e.Q != null) {
-
                 for (var j=0;j<e.Q.length;j++) {
+
                     // free kick: start new block
                     if (e.Q[j]._qualifier_id == 5) {
                         matchBlocks.push(curBlock);
-                        curBlock = {touches:[]};
+                        curBlock = {touches:[],_team_id:0};
                         continue;
                     }
                     // throw in: start new block
-                    else if (e.Q[j]._qualifier_id == 131) {
+                    else if (e.Q[j]._qualifier_id == 107) {
                         matchBlocks.push(curBlock);
-                        curBlock = {touches:[]};
+                        curBlock = {touches:[],_team_id:0};
+
                         continue;
                     }
+                }
             }
+            // if it's a pass, and successful, and by a new team
+            if (e._type_id == 1 && e._outcome && e._team_id != curBlock._team_id) { 
+                matchBlocks.push(curBlock);
+                curBlock = {touches:[],_team_id: e._team_id };
+                
+                continue;
             }
-
-            if (matchEvents.Event[i]._type_id == 4) { // if need new block
+            /*if (e._type_id == 99994) { // if need new block
 
                 curBlock.touches.push(matchEvents.Event[i]);
                 matchBlocks.push(curBlock);
-                curBlock = {touches:[]};
+                curBlock = {touches:[],_team_id:0};
                 continue;
-            } 
+            }*/ 
 
                 curBlock.touches.push(matchEvents.Event[i]);
 
@@ -56,6 +72,8 @@ function matchSplitter() {
 		//var oneBlock = {touches:[matchEvents.Event[0],matchEvents.Event[1]]};
 		//var twoBlock = {touches:[matchEvents.Event[2],matchEvents.Event[3]]};
 		//var matchBlocks = [oneBlock,twoBlock];
+        
+        matchBlocks.push(curBlock);
 		return matchBlocks;
 	}
 	
