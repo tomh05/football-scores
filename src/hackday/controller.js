752c5b4f22b675c7f4167f8fbb9ac89de8792e35
Paul Lemon <paul.lemon@gmail.com>
2013-12-19 14:35:28 +0000

    framework

--- src/hackday/controller.js
+++ src/hackday/controller.js
@@ -3,6 +3,8 @@
 	
 	
 	app.factory('scoreTouches', [scoreTouches]);
+	app.factory('matchSplitter', [matchSplitter]);
+	
 	
 	// controllers are connected to url routes inside src/app.js
 	app.controller('hackdayIndexController',
@@ -16,35 +18,57 @@
 	
 	
 	app.controller('hackdayMatchSplitterController',
-		['matchService','$routeParams','$scope', 'commonViewFunctionality','matchCommonFunctionality',
-		function(matchService,$routeParams,$scope,commonViewFunctionality,matchCommonFunctionality) {
+		['matchService','$routeParams','$scope', 'commonViewFunctionality','matchCommonFunctionality','matchSplitter',
+		function(matchService,$routeParams,$scope,commonViewFunctionality,matchCommonFunctionality,matchSplitter) {
 			$scope.matchId = $routeParams.matchid;
 			commonViewFunctionality.initCommonFunctions($scope);
 			matchService.loadMatch($scope.matchId);
+			
+			$scope.$on('matchEventsLoaded', function(e){ 
+				var matchBlocks = matchSplitter.execute(matchService.getEvents());
+				$scope.matchBlocks =matchBlocks;
+			});
+			
 			$scope.world="Splitter!";
 	}]);
 	
 	
 	app.controller('hackdayBlockScorerController',
-		['matchService','$routeParams','$scope', 'commonViewFunctionality','matchCommonFunctionality',
-		function(matchService,$routeParams,$scope,commonViewFunctionality,matchCommonFunctionality) {
+		['matchService','$routeParams','$scope', 'commonViewFunctionality','matchCommonFunctionality','matchSplitter','blockScorer'
+		function(matchService,$routeParams,$scope,commonViewFunctionality,matchCommonFunctionality,matchSplitter,blockScorer) {
 			$scope.matchId = $routeParams.matchid;
 			commonViewFunctionality.initCommonFunctions($scope);
 			matchService.loadMatch($scope.matchId);
 			$scope.world="Scorer!";
+			
+			$scope.$on('matchEventsLoaded', function(e){ 
+				var matchBlocks = matchSplitter.execute(matchService.getEvents());
+				var blockScores = [];
+				for (var matchBlock in matchBlocks) {
+					var blockScore = blockScorer.execute(matchBlock);
+					blockScores.push(blockScore);
+				}
+				$scope.blockScores =blockScores;
+			});
 	}]);
 	
 	app.controller('hackdayScoreTouchController',
-		['matchService','$routeParams','$scope', 'commonViewFunctionality','matchCommonFunctionality','scoreTouches',
+		['matchService','$routeParams','$scope', 'commonViewFunctionality','matchCommonFunctionality','scoreTouches','matchSplitter','blockScorer',
 		function(matchService,$routeParams,$scope,commonViewFunctionality,matchCommonFunctionality,scoreTouches) {
 			$scope.matchId = $routeParams.matchid;
 			commonViewFunctionality.initCommonFunctions($scope);
 			matchService.loadMatch($scope.matchId);
-			$scope.world="TouchScorer!";
-			scoreTouches.setProp1(5);
-			scoreTouches.incrementProp1();
-			scoreTouches.incrementProp1();
-			$scope.prop1=scoreTouches.getProp1();
+			
+			$scope.$on('matchEventsLoaded', function(e){ 
+				var matchBlocks = matchSplitter.execute(matchService.getEvents());
+				var touchScores = [];
+				for (var matchBlock in matchBlocks) {
+					var blockScore = blockScorer.execute(matchBlock);
+					var scoredTouch = scoreTouches.execute(matchBlock,blockScore);
+					touchScores.push(scoredTouch);
+				}
+				$scope.blockScores =touchScores;
+			});
 	}]);
 	
 	app.controller('hackdayPlayerAggregateScoresController',
