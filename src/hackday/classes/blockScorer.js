df0e601ccf07e828764ab7cd623c59fc5e992486
laurahk <Laura.Howarth-Kirke@bbc.co.uk>
2013-12-20 10:01:08 +0000

    pseudocode

--- src/hackday/classes/blockScorer.js
+++ src/hackday/classes/blockScorer.js
@@ -11,20 +11,35 @@ function blockScorer() {
 
 	function execute(matchBlock) {
 		var blockScore = {score:10};
+
+		/*
+
+		teamWithPossesion = matchBlock.team_id;
+		lastTouch = iterate backwards to find last touch by teamWithPossession
+		if (lastTouch == goal)
+			score = 10;
+		else
+			if (positionInField == onOtherTeamsSide ( /10)
+				score = 2;
+			else
+				score = 0;
+
+		*/
+
 		return blockScore;
 	}
-	
+
 	//implementation - unless returned above the methods are not accessible
-	
+
 	var prop1 = 0;
-	
+
 	function getProp1() {
 		return prop1;
-	}	
-	
+	}
+
 	function setProp1(value) {
 		prop1 = value;
-	}	
+	}
 	function incrementProp1() {
 		prop1++;
 	}
