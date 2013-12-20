a8b5273390c0a4c996271ecc71a468dead8c8e5d
Paul Lemon <paul.lemon@gmail.com>
2013-12-20 12:04:04 +0000

    Tweaking the awards of score touches to the opposing team in a block

--- src/hackday/classes/scoreTouches.js
+++ src/hackday/classes/scoreTouches.js
@@ -16,16 +16,27 @@ function scoreTouches() {
 		var score = blockScore.score;
 		var percentage = 1;
 		var totalTouches =  matchBlock.touches.length-1;
+		var currentScoredTouchNumber = totalTouches;
 		for (var touchIndex= totalTouches; percentage>0 && touchIndex>=0; touchIndex--) {
 			var touch = matchBlock.touches[touchIndex];
-			var touchScore = {touch:touch,score:0, percentage:percentage};
-			if (touch._team_id == possesionTeamId) {
-				touchScore.score=score*percentage;
-			} else if (touchIndex==matchBlock.touches.length-1) {
-				touchScore.score=score*percentage;
+			var touchScore = {touch:touch,score:0, percentage:0,awardRatio:0};
+			if (touch._type_id != 5 && touch._type_id != 49) {
+				var awardScore = false;
+				var awardRatio = 1;
+				if (touch._team_id == possesionTeamId) {
+					awardScore = true;
+				} else if (touch._outcome==1) {
+					awardScore = true;
+				}
+				if (awardScore) {
+					touchScore.percentage = percentage;
+					touchScore.awardRatio = awardRatio;
+					touchScore.score=score*percentage*awardRatio;
+					percentage = calculatePercentage(percentage,currentScoredTouchNumber,totalTouches);
+					currentScoredTouchNumber--;
+				}
 			}
 			touchScores.scores[touchIndex]=touchScore;
-			percentage = calculatePercentage(percentage,touchIndex,totalTouches);
 		}
 		return touchScores;
 	}
